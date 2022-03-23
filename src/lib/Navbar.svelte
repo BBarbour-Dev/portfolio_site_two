5af122dd3d0d4d384f0dc4072d41e348264edcd0
Brian <steelvoltage@gmail.com>
2022-03-23 11:40:36 -0400

    fixed resume link

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,7 +3,7 @@
 	<div class="nav-right">
 		<a href="/articles">Articles</a>
 		<a href="/projects">Projects</a>
-		<a href="/resume">Resume</a>
+		<a href="https://drive.google.com/file/d/1kV6Lo8Md7BCJ_Fe7pzGxReVKGyZsHNvr/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
 	</div>
 </nav>
 
